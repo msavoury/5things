c0905b14f9761383bc6331c3a68c161a58e55758
Marcos Savoury <marcossavoury@hotmail.com>
2013-12-30 03:07:43 -0500

    Basic setup of 2 player page

--- client/views/application/game.js
+++ client/views/application/game.js
@@ -1,5 +1,25 @@
 Template.game.helpers({
-	username: function () {
+	logged_in_username: function () {
 		return Session.get('user.name');
+	},
+
+    game_is_ready: function(game) {
+		return this.user_count > 1;
+	},
+
+});
+
+Template.gameAction.helpers({
+	//TODO: remove the duplication of this function 
+	logged_in_username: function () {
+		return Session.get('user.name');
+	},
+	other_username: function() {
+		if (this.users[0].username == Session.get('user.name')) {
+			return this.users[1].username;
+		}
+		else {
+			return this.users[0].username;
+		}
 	}
 });
