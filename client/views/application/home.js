81a3771f57732fbb5041ea32e2d4cb21a09298d6
Marcos Savoury <marcossavoury@hotmail.com>
2013-12-30 01:54:09 -0500

    Adding ability to redirect to game with id in url

--- client/views/application/home.js
+++ client/views/application/home.js
@@ -2,6 +2,11 @@ function has_username() {
 	return Session.get('user.id') != "" && Session.get('user.id') != undefined;
 }
 
+function User(username) {
+	this.username = username;
+	this.status = 'idle';
+}
+
 if (Meteor.isClient) {
   Template.home.events({
     'click input' : function () { }
@@ -9,6 +14,7 @@ if (Meteor.isClient) {
 
   Template.home.helpers( {
 	  has_username: function() { return false; },
+	  num_of_active_games: function() {return 4;},
   });
 
   Template.enter_username.events( {
@@ -18,15 +24,20 @@ if (Meteor.isClient) {
 			  alert('please enter username');
 			  return;
 		  }
+		  var new_user;
 		  if (!has_username() && username != "") {
-			  var user_id = Players.insert({username: username, status:'idle'});
+			  //var user_id = Players.insert({username: username, status:'idle'});
+			  new_user = new User(username);
+			  var user_id = Players.insert(new_user);
 			  Session.set('user.id', user_id);
-			  Session.set('user.name', username);
+			  Session.set('user.name', new_user.username);
 		  }
 
 		  if (has_username()) {
-			  //find a game and go to
-			  //Router.go('lobby');
+			  Meteor.call('assign_user_to_game', new_user, function (error, game_id) {
+				  //alert(game_id);
+				  Router.go('game', {_id:game_id});
+			  });
 		  }
 	  },
 
--- collections/games.js
+++ collections/games.js
@@ -3,3 +3,39 @@ Games = new Meteor.Collection('games');
 function user_can_play() {
 	return true;
 }
+
+function Game() {
+	this.users = [];
+    this.user_count = 0; 
+
+	this.add_user = function(user_id) {
+		this.users.push(user_id);
+		this.user_count++;
+	};
+}
+
+function add_user_to_game(game, user) {
+	game.users.push(user);
+	game.user_count++;
+}
+
+Meteor.methods({
+	assign_user_to_game: function(user) {
+		//find a game that has one user assigned
+		var target_game = Games.findOne({user_count: {'$gt':0}});
+
+		if (target_game != undefined) {
+			add_user_to_game(target_game, user);
+			//Games.update(target_game);
+			return target_game._id;
+		}
+		else {
+			var new_game =  new Game();
+			new_game.add_user(user);
+			var game_id = Games.insert(new_game);
+			return game_id;
+		}
+	},
+
+
+});
