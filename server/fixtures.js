2398f1b01bf0b90ad8f204ca54819369c1d70f58
Marcos Savoury <marcossavoury@hotmail.com>
2013-12-31 04:51:20 -0500

    Removing fake players, adding new questions

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,42 +1,29 @@
 
-var fake_players = [
-    {   
-        username: 'carine',
-        status: 'idle',
-    },  
-    {   
-        username: 'marcos',
-        status: 'idle',
-    },  
-    {   
-        username: 'sergio',
-        status: 'active',
-    },  
-    {   
-        username: 'kristine',
-        status: 'active',
-    },  
-
-]
-
 var questions = [
-{ text: 'Name 5 of the original 13 colonies',
+{ text: 'Name 5 of the original 13 US colonies',
   answers: ['virginia','massachusetts','new hampshire','maryland','connecticut','rhode island','delaware','north carolina','south carolina','new jersey','new york','pennsylvania','georgia'],
-
 },
 {
   text: 'Name the 5 great lakes',
   answers: ['huron', 'ontario', 'michigan', 'erie', 'superior'],
 
 },
+{
+  text: 'Name 5 states that start with the letter "M"',
+  answers: ['maine', 'maryland', 'massachusetts', 'michigan', 'minnesota','mississippi','missouri','montana'],
+},
+{
+  text: 'Name 5 of Snow White\'s Seven Dwarfs',
+  answers: ['doc', 'dopey', 'bashful', 'grumpy', 'sneezy','sleepy','happy'],
+},
+{
+  text: 'Name 5 of Santa Claus\' Reindeer',
+  answers: ['dasher', 'dancer', 'prancer', 'vixen', 'comet','cupid','donner','blitzen','rudolph'],
+},
 
 ]
 
 Meteor.startup(function() {
-	if (Players.find().count() === 0) {
-		fake_players.forEach(function(entry) {Players.insert(entry);});
-	}
-
 	if (Questions.find().count() === 0) {
 		questions.forEach(function(entry) {Questions.insert(entry);});
 	}
