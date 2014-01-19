ae31fb2e89eddba985fc3d58efb788df2136fe82
Marcos Savoury <marcossavoury@hotmail.com>
2014-01-19 14:40:46 -0500

    Sign in via pressing enter key on homepage

--- client/views/application/home.js
+++ client/views/application/home.js
@@ -7,6 +7,28 @@ function User(username) {
 	this.status = 'idle';
 }
 
+function sign_in() {
+	var username = $('#username').val();
+	if (username == undefined || username == "") {
+		alert('please enter username');
+		return;
+	}
+	var new_user;
+	if (!has_username() && username != "") {
+		new_user = new User(username);
+		var user_id = Players.insert(new_user);
+		Session.set('user.id', user_id);
+		Session.set('user.name', new_user.username);
+	}
+
+	if (has_username()) {
+		new_user = Players.findOne(user_id);
+		Meteor.call('assign_user_to_game', new_user, function (error, game_id) {
+			Router.go('game', {_id:game_id});
+		});
+	}
+}
+
 if (Meteor.isClient) {
   Template.home.events({
     'click input' : function () { }
@@ -18,30 +40,10 @@ if (Meteor.isClient) {
   });
 
   Template.enter_username.events( {
-	  'click input#select_username' : function () {
-		  var username = $('#username').val();
-		  if (username == undefined || username == "") {
-			  alert('please enter username');
-			  return;
-		  }
-		  var new_user;
-		  if (!has_username() && username != "") {
-			  //var user_id = Players.insert({username: username, status:'idle'});
-			  new_user = new User(username);
-			  var user_id = Players.insert(new_user);
-			  Session.set('user.id', user_id);
-			  Session.set('user.name', new_user.username);
-		  }
-
-		  if (has_username()) {
-			  new_user = Players.findOne(user_id);
-			  Meteor.call('assign_user_to_game', new_user, function (error, game_id) {
-				  //alert(game_id);
-				  Router.go('game', {_id:game_id});
-			  });
-		  }
+	  'click input#select_username' : function (event) {sign_in();},
+	  'keydown input#username' : function(event) {
+		  if (event.which == 13) sign_in();
 	  },
-
   });
 }
 
